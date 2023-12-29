c729bdd3a3c3cb66530e8e40c910a28ccca9bc88
ae5n <nuehsan@yahoo.com>
2023-12-29 13:47:14 -0600

    Added new attributes

--- main.py
+++ main.py
@@ -7,25 +7,9 @@ import time
 def load_data(filepath: str) -> pd.DataFrame:
     return pd.read_csv(filepath)
 
-def classify(report: str, api_key: str) -> str:
-    instruct_prompt = f"""
-    Task: Classify the provided construction accident report. Determine the cause of the accident, which may be one of: electrocution, struck, fall, or caught.
-
-    Further, classify the severity of the incident:
-
-    Fatal: The accident resulted in the death of the individual(s) involved.
-    Nonfatal: The accident resulted in injury, but did not lead to death.
-    
-    Your response should be exactly one of the following options, without additional explanations:
-
-    electrocution_nonfatal
-    struck_nonfatal
-    fall_nonfatal
-    caught_nonfatal
-    electrocution_fatal
-    struck_fatal
-    caught_fatal
-    fall_fatal
+def classify(report: str, api_key: str, instruct_prompt: str) -> str:
+    full_prompt = f"""
+    Task: {instruct_prompt}
     ---
     Report:
     {report}
@@ -36,7 +20,7 @@ def classify(report: str, api_key: str) -> str:
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": instruct_prompt}
+                {"role": "user", "content": full_prompt}
             ])
         
         return completion.choices[0].message.content
@@ -46,55 +30,81 @@ def classify(report: str, api_key: str) -> str:
         return "error"
 
 def main():
-    all_classes = [
-        'electrocution_nonfatal',
-        'struck_nonfatal',
-        'fall_nonfatal',
-        'caught_nonfatal',
-        'electrocution_fatal',
-        'struck_fatal',
-        'caught_fatal',
-        'fall_fatal'
-    ]
-
-    data_path = 'Data/cleaned_data_25.csv'
+    data_path = 'labeled_data.csv'
     test_data = load_data(data_path)
+
+    actual_injury_cause_labels = test_data['Injury Cause'].tolist()
+    actual_root_cause_labels = test_data['Root Cause'].tolist()
+    actual_severity_labels = test_data['Severity'].tolist()
+    actual_body_part_labels = test_data['Body Part'].tolist()
+    actual_accident_time_labels = test_data['Accident Time'].tolist()
 
     api_key = os.environ.get('OPENAI_API_KEY')
     if not api_key:
         raise ValueError("OPENAI_API_KEY not found in environment variables.")
     openai.api_key = api_key
-
-    actual_labels = []
-    predicted_labels = []
+    
     reports = []
-    BATCH_SIZE = 50
+
+    injury_cause_labels = []
+    root_cause_labels = []
+    severity_labels = []
+    body_part_labels = []
+    accident_time_labels = []
+
+    injury_cause_prompt = """Determine the Injury Cause of the accident in the report. Your answer should be strictly one of the following: 'Electrocution', 'Struck by', 'Fall', or 'Caught in/between' without any additional text or explanations."""
+        
+    root_cause_prompt = """Determine the Root Cause of the accident in the report. Your answer should be strictly one of the following: 'Struck by', 'Caught in/between', 'Fall', 'Electrocution', or 'Unspecified' without any additional text and explanations."""
+
+    severity_prompt = """Determine the Severity of the incident in the report. Your answer should be strictly one of the following: 'Fatal' or 'Nonfatal' without any additional text or explanations."""
+
+    body_part_prompt = """Determine the main Body Part affected in the accident. Provide only and strictly the main body part affected without any additional text, explanations, or multiple body parts. If the information is not available, say 'Unspecified'."""
+
+    accident_time_prompt = """Determine the Accident Time of the accident in the report. The answer should strictly be in the format HH:MM am/pm. If the information is not available, say 'Unspecified'. Do not include the date and any other additional text and explanations."""
+
+    BATCH_SIZE = 10
     for i in range(0, len(test_data), BATCH_SIZE):
         batch = test_data.iloc[i:i+BATCH_SIZE]
         print(f"Processing batch starting from index {i}")
         for _, row in batch.iterrows():
             report = row["Report"]
-            actual_label = row["Label"]
-            predicted_label = classify(report, api_key)
-            actual_labels.append(actual_label)
-            predicted_labels.append(predicted_label)
+            injury_cause = classify(report, api_key, injury_cause_prompt)
+            root_cause = classify(report, api_key, root_cause_prompt)
+            severity = classify(report, api_key, severity_prompt)
+            body_part = classify(report, api_key, body_part_prompt)
+            accident_time = classify(report, api_key, accident_time_prompt)
+            time.sleep(3)
+            injury_cause_labels.append(injury_cause)
+            root_cause_labels.append(root_cause)
+            severity_labels.append(severity)
+            body_part_labels.append(body_part)
+            accident_time_labels.append(accident_time)
             reports.append(report)
-        time.sleep(6)
+        # time.sleep(6)
 
-    accuracy = accuracy_score(actual_labels, predicted_labels)
-    conf_matrix = confusion_matrix(actual_labels, predicted_labels, labels=all_classes)
+    # accuracy = accuracy_score(actual_labels, predicted_labels)
+    # conf_matrix = confusion_matrix(actual_labels, predicted_labels, labels=all_classes)
 
     results_df = pd.DataFrame({
-        'Actual Label': actual_labels,
-        'Predicted Label': predicted_labels,
+        'Actual Injury Cause': actual_injury_cause_labels,
+        'Predicted Injury Cause': injury_cause_labels,
+        'Actual Root Cause': actual_root_cause_labels,
+        'Predicted Root Cause': root_cause_labels,
+        'Actual Severity': actual_severity_labels,
+        'Predicted Severity': severity_labels,
+        'Actual Body Part': actual_body_part_labels,
+        'Predicted Body Part': body_part_labels,
+        'Actual Accident Time': actual_accident_time_labels,
+        'Predicted Accident Time': accident_time_labels,
         'Report': reports
     })
+
     results_file_path = "classification_results.csv"
     results_df.to_csv(results_file_path, index=False)
 
-    print("Accuracy:", accuracy)
-    print("Confusion Matrix:\n", conf_matrix)
-    print("Results saved to:", results_file_path)
+    # print("Accuracy:", accuracy)
+    # print("Confusion Matrix:\n", conf_matrix)
+    # print("Results saved to:", results_file_path)
 
 if __name__ == '__main__':
     main()
